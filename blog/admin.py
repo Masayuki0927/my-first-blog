3f3f567f612fedd821f158e0db5f1f1dd3b4d112
Masayuki0927 <masayuki.0927.ikeda@gmail.com>
2021-11-14 13:53:16 +0900

    Added views to create/edit blog post inside the site.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Post
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_date')  # 一覧に出したい項目
+
 admin.site.register(Post)
